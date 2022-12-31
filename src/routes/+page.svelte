49c5fb7f4bed8a2d2cb73d007ae67ac23cb5c468
Bhavya Muni <bhavyamuni@outlook.com>
2022-12-31 00:20:12 -0500

    got it looking nice?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col h-screen justify-end">
+	<div class="flex flex-col w-1/3 my-auto justify-center mx-auto leading-relaxed space-y-3">
+		<h1 class="font-semibold text-2xl">Hey, there 👋🏼</h1>
+		<p />
+
+		<p class="">I’m Bhavya, welcome to my world!</p>
+		<p>
+			When I’m bored, I learn new tech. I started off making games in and surprisingly won the first
+			ever game jam that I participated in (the game and it’s showcase). After creating more games
+			for a couple of years I discovered Flutter. Launched an app with some friends for connecting
+			musicians which got more than a thousand downloads on the play store.
+		</p>
+		<p>
+			More recently, I have been exploring machine learning. Plopped a bunch of dinos in the Chrome
+			Dinosaur Game to learn to play the game themselves. Currently working on a project to create
+			fake songs based on styles of top artists.
+		</p>
+		<p>
+			Apart from tech I am also into other nerdy stuff like aerospace, currently working on a design
+			team to build a rocket engine (no, we haven’t blown up anything as of yet). But above all
+			comes my love for COFFEE (yes, I have a coffee journal too).
+		</p>
+	</div>
+
+	<div class="h-2 bg-gradient-to-r from-orange-400 to-blue-500 w-screen" />
+</div>
