8b9a07fca347fcd01ed77ad052b8e70e70059da6
Bhavya Muni <bhavyamuni@outlook.com>
2022-12-31 11:42:46 -0500

    updatess

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,27 @@
 <div class="flex flex-col h-screen justify-end">
 	<div class="flex flex-col lg:w-1/3 sm:w-2/3 justify-center m-auto leading-relaxed space-y-3">
-		<h1 class="font-semibold text-2xl">Hey, there 👋🏼</h1>
+		<div class="flex flex-row justify-between items-center">
+			<h1 class="font-semibold text-3xl">Hey, there 👋🏼</h1>
+			<!-- <img src="bitmoji.png" class="w-1/5 h-auto" alt="bitmoji" /> -->
+		</div>
 		<p />
 
 		<p class="">I’m Bhavya, welcome to my world!</p>
 		<p>
-			When I’m bored, I learn new tech. I started off making games in and surprisingly won the first
-			ever game jam that I participated in (the game and it’s showcase). After creating more games
-			for a couple of years I discovered Flutter. Launched an app with some friends for connecting
-			musicians which got more than a thousand downloads on the play store.
+			When I’m bored, I learn new tech. I started off making games and surprisingly won the first
+			ever game jam that I participated in (<a href="">it was lit</a>). After creating more games
+			for a couple of years I discovered Flutter. Launched <a href="">an app </a> with some friends
+			for connecting musicians which got <em> more than a thousand downloads</em> on the play store.
 		</p>
 		<p>
 			More recently, I have been exploring machine learning. Plopped a bunch of dinos in the Chrome
-			Dinosaur Game to learn to play the game themselves. Currently working on a project to create
-			fake songs based on styles of top artists.
+			Dinosaur Game to learn to play <a href="">the game</a> themselves. Currently working on
+			<a href="">a project</a> to create fake songs based on styles of top artists.
 		</p>
 		<p>
 			Apart from tech I am also into other nerdy stuff like aerospace, currently working on a design
-			team to build a rocket engine (no, we haven’t blown up anything as of yet). But above all
-			comes my love for COFFEE (yes, I have a coffee journal too).
+			team to build <a href="/">a rocket engine</a> (no, we haven't blown up anything as of yet).
+			But above all comes my love for COFFEE (yes, I have <a href="/">a coffee journal</a> too).
 		</p>
 	</div>
 
