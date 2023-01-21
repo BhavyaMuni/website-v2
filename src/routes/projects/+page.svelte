44f2b9810a5e265ad7221a5f1e1dc08470a83074
Bhavya Muni <bhavyamuni@outlook.com>
2023-01-20 21:32:07 -0500

    title tag

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,7 +3,7 @@
 		class="flex flex-col lg:w-1/3 sm:w-2/3 xs:w-2/3 justify-center m-auto leading-relaxed space-y-3"
 	>
 		<div class="flex flex-row justify-start items-center">
-			<a href="/"><i class="fa-solid fa-chevron-left fa-xl" /></a>
+			<a href="/"><i class="fas fa-chevron-left fa-xl" /></a>
 			<div class="w-2" />
 			<h1 class="font-semibold text-3xl">Projects 👾</h1>
 			<!-- <img src="bitmoji.png" class="w-1/5 h-auto" alt="bitmoji" /> -->
