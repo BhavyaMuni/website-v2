7b4506440a5869dc9e59414cd63b1a068de4888b
Bhavya Muni <bhavyamuni@outlook.com>
2023-03-21 20:08:37 -0400

    animated gradients

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,12 @@
 <div class="flex flex-col items-center">
-	<p class="opacity-40 text-xs">made with lots of ☕️</p>
-	<div class="h-2" />
-	<div class="h-2 bg-gradient-to-r from-orange-400 to-blue-500 w-screen" />
+	<p class="opacity-40 text-xs py-2">made with lots of ☕️</p>
+	<div
+		class="h-2 animate-move-gradient bg-gradient-to-r from-orange-400 via-blue-500 to-orange-400 w-full"
+	/>
 </div>
+
+<style>
+	.animate-move-gradient {
+		background-size: 400%;
+	}
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,18 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				gradient: {
+					'0%': { 'background-position': '0% 50%' },
+					'50%': { 'background-position': '100% 50%' },
+					'100%': { 'background-position': '0% 50%' }
+				}
+			},
+			animation: {
+				'move-gradient': 'gradient 10s ease infinite'
+			}
+		},
 		fontFamily: {
 			sans: ['Inter', ...defaultTheme.fontFamily.sans]
 		},
