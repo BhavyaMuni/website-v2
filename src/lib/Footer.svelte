73db1fe8b2ff3cf8dceca8d9101dd7037d2e48a7
Bhavya Muni <bhavyamuni@outlook.com>
2023-03-28 19:22:21 -0400

    working transition of gradient based on mouse

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,12 +1,24 @@
+<script>
+	import { spring } from 'svelte/motion';
+	let innerWidth = 10;
+	let mx = spring(0, { stiffness: 0.03, damping: 0.9 });
+	function handleMousemove(event) {
+		mx.set((event.clientX / innerWidth) * 100, { duration: 50 });
+	}
+</script>
+
+<svelte:window on:mousemove={handleMousemove} bind:innerWidth />
 <div class="flex flex-col items-center">
 	<p class="opacity-40 text-xs py-2">made with lots of ☕️</p>
 	<div
-		class="h-2 animate-move-gradient bg-gradient-to-r from-orange-400 via-blue-500 to-orange-400 w-full"
+		class="h-2 move-gradient bg-gradient-to-r from-orange-400 via-blue-500 to-orange-400 w-full"
+		style="--mx: {$mx}%"
 	/>
 </div>
 
 <style>
-	.animate-move-gradient {
+	.move-gradient {
 		background-size: 400%;
+		background-position: var(--mx) 0%;
 	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,6 @@
 
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
-
 	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
