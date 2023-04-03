c5b46f51e1c8a0b58a2c625dd39639780aeacd9d
Bhavya Muni <bhavyamuni@outlook.com>
2023-04-03 16:05:59 -0400

    dark mode should be working 🥳 (#5)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,27 +7,54 @@
 	import '@fontsource/inter/600.css';
 	import '@fontsource/inter/700.css';
 	import '@fontsource/inter/800.css';
-	import Footer from '$lib/Footer.svelte';
+
 	import Social from '$lib/Social.svelte';
 	import { spring } from 'svelte/motion';
 
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics';
-	inject({ mode: dev ? 'development' : 'production' });
+	// import { dev } from '$app/environment';
+	// import { inject } from '@vercel/analytics';
+	// inject({ mode: dev ? 'development' : 'production' });
 	let innerWidth;
 	let innerHeight;
 	let mx = spring(0, { stiffness: 0.1, damping: 0.95 });
 	let my = spring(0, { stiffness: 0.1, damping: 0.95 });
 	function handleMousemove(event) {
-		mx.set(innerWidth - event.clientX, { duration: 1000 });
-		my.set(innerHeight - event.clientY, { duration: 1000 });
+		mx.set(innerWidth - event.clientX);
+		my.set(innerHeight - event.clientY);
+	}
+
+	import { browser } from '$app/environment';
+
+	let darkMode = false;
+
+	function handleSwitchDarkMode() {
+		darkMode = !darkMode;
+
+		localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+
+		darkMode
+			? document.documentElement.classList.add('dark')
+			: document.documentElement.classList.remove('dark');
+	}
+
+	if (browser) {
+		if (
+			localStorage.theme === 'dark' ||
+			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+		) {
+			document.documentElement.classList.add('dark');
+			darkMode = true;
+		} else {
+			document.documentElement.classList.remove('dark');
+			darkMode = false;
+		}
 	}
 </script>
 
 <svelte:window on:mousemove={handleMousemove} bind:innerWidth bind:innerHeight />
 <div
-	class="flex flex-col h-screen justify-between bg-grad items-center"
-	style="--mx: {$mx}px; --my: {$my}px"
+	class="flex flex-col h-screen justify-between bg-grad dark:bg-grad-dark items-center dark:text-white "
+	style="--mx: {$mx}px; --my: {$my}px;"
 >
 	<div
 		class="flex flex-col lg:w-1/3 sm:w-2/3 xs:w-4/5 justify-center m-auto py-12 leading-relaxed space-y-3"
@@ -35,13 +62,14 @@
 		<slot />
 	</div>
 	<Social />
-
-	<p class="opacity-40 text-xs py-4">made with lots of ☕️</p>
+	<div class="opacity-60 flex flex-col sm:self-stretch sm:flex-row">
+		<p class="text-xs py-4 sm:ml-auto">made with lots of ☕️</p>
+		<button on:click={handleSwitchDarkMode} class="sm:ml-auto sm:mr-4">
+			{#if darkMode}
+				<i class="far fa-sun" />
+			{:else}
+				<i class="far fa-moon" />
+			{/if}
+		</button>
+	</div>
 </div>
-
-<style>
-	.bg-grad {
-		background: radial-gradient(circle at var(--mx) var(--my), white 70%, transparent 125%),
-			linear-gradient(to right, rgb(253, 186, 116), rgb(253, 164, 175));
-	}
-</style>
